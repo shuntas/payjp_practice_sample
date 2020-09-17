e335118ca4408a536e1c2f36142ffca973ef69c3
shuntas <shunta.suzuki@di-v.co.jp>
2020-09-17 17:53:43 +0900

    first commit

---
+++ app/javascript/card.js
@@ -0,0 +1,36 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_92ec0a93dd010cd09e3c55cf"); // PAY.JPテスト公開鍵
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+ 
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+ 
+    const card = {
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+      
+      document.getElementById("order_number").removeAttribute("name");
+      document.getElementById("order_cvc").removeAttribute("name");
+      document.getElementById("order_exp_month").removeAttribute("name");
+      document.getElementById("order_exp_year").removeAttribute("name");
+ 
+      document.getElementById("charge-form").submit();
+      document.getElementById("charge-form").reset();
+    });
+  });
+ };
+ 
+ window.addEventListener("load", pay);
